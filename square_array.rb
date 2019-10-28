bf1307db5b21dc344fab8ee971fc000a89907c64
Tommy Connor <tconnor98@aol.com>
2019-10-28 01:19:23 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@ def square_array(numbers)
   new_array = []
   counter = 0
   
- while numbers[counter] do |number|
-    new_array << number ** 2
+ while numbers[counter] do
+    new_array << numbers[counter] ** 2
   end
 end
 
