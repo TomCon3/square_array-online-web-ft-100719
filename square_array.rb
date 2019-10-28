a53963751191202732b8d7ccf271c158bc756805
Tommy Connor <tconnor98@aol.com>
2019-10-28 01:28:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(numbers)
   new_array = []
-  numbers.each do |number|
+  new_array >> numbers.each do |number|
     number ** 2
-    new_array >> 
   end
+  return new_array
 end
 
 
